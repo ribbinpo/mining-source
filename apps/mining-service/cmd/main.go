12f2ba54f50906e6c5bad2e6ab7cf299e074eb25
Teerawut Saesim <ribbinpo@gmail.com>
2025-07-27 02:45:58 +0700

    feat: implement main to execute program

--- apps/mining-service/cmd/main.go
+++ apps/mining-service/cmd/main.go
@@ -4,15 +4,33 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/ribbinpo/mining-service/internal/application/usecase"
+	"github.com/ribbinpo/mining-service/internal/config"
+	"github.com/ribbinpo/mining-service/internal/frameworks/database"
+	repository "github.com/ribbinpo/mining-service/internal/frameworks/database/repository"
+	"github.com/ribbinpo/mining-service/internal/frameworks/webscraping"
+	"github.com/ribbinpo/mining-service/internal/frameworks/webservice"
+	"github.com/ribbinpo/mining-service/internal/frameworks/webservice/router"
 )
 
 func main() {
-	fiberApp := fiber.New()
+	config := config.NewConfig(".env")
+	webService := webservice.NewWebService(config.App.Port)
 
-	fiberApp.Get("/", func(c *fiber.Ctx) error {
+	api := webService.FiberApp.Group("/api")
+
+	databaseInstance := database.NewDatabase(config.Db.Dsn)
+	db := databaseInstance.Connect()
+	scraperRepository := webscraping.NewScraperRepository()
+	stoneRepository := repository.NewStoneRepository(db)
+	stoneUsecase := usecase.NewStoneUsecase(scraperRepository, stoneRepository)
+
+	router.StoneRouter(api, stoneUsecase)
+
+	webService.FiberApp.Get("/", func(c *fiber.Ctx) error {
 		fmt.Println("Mining service is running")
 		return c.SendString("Mining service is running")
 	})
 
-	fiberApp.Listen(":4000")
+	webService.Start()
 }
